a40834526f558d92bc83f9795e8be9ddf2ea0f4d
Tanjiro2010 <himanshujsr462@gmail.com>
2023-10-26 20:18:42 +0530

    just initial code of my web journey
    initial web dev code

---
+++ script.js
@@ -0,0 +1,18 @@
+try {
+    // Code that might throw an error
+    console.log("This is a debug message.");
+} catch (error) {
+    console.error("An error occurred: " + error.message);
+}
+// Example: Load content dynamically
+function loadContent() {
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", "index.html", true);
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState == 4 && xhr.status == 200) {
+            document.getElementById("content").innerHTML = xhr.responseText;
+        }
+    };
+    xhr.send();
+}
+
